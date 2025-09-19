c04074286315b8b65862b8c436b6081db4ad00ee
MrRajKumar07 <rajkumar951ce@gmail.com>
2025-09-19 22:51:16 +0530

    Initial commit

---
+++ new.sql
@@ -0,0 +1,6 @@
+insert into customers(id, first_name, country, score) values
+    -> (2, 'Yona Sherpa','Hong Kong',900),
+    -> (3, 'Jagrit Kumar Singh,'Canada',560),
+    '> (4,'Deepak Mandal','India',770),
+    '> (5,'Chandan Kumar','Germany',600),
+    '> (5,'Chandan Kumar','Germany',600),
